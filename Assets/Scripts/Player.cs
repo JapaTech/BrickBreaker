10cd8a5e2e8d1773a88ff151f0123ed7a649d6b3
Jonathan Iha <58711422+JapaTech@users.noreply.github.com>
2024-08-08 13:23:41 -0300

    Muda o ângulo da bola
    Criada a interação que muda o ângulo da bola de acordo com a posição que atingiu o jogador a atingiu

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,8 @@ public class Player : MonoBehaviour
     private float inputValue;
     private Vector2 movement;
 
+    [SerializeField] private float maxBounceAngle = 75;
+
     private void Awake()
     {
         playerInput = new PlayerInput();
@@ -35,5 +37,22 @@ public class Player : MonoBehaviour
         movement.x = inputValue;
         movement.y = 0;
         rb.velocity = movement * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            Rigidbody2D ballRb = collision.rigidbody;
+            Collider2D paddleCol = collision.otherCollider;
+
+            Vector2 ballDirection = ballRb.velocity.normalized;
+            Vector2 contactDistance = paddleCol.bounds.center - ballRb.transform.position;
+
+            float bounceAngle = (contactDistance.x / paddleCol.bounds.size.x) * maxBounceAngle;
+            ballDirection = Quaternion.AngleAxis(bounceAngle, Vector3.forward) * ballDirection;
+
+            ballRb.velocity = ballRb.velocity.magnitude * ballDirection;
+        }
     }
 }
