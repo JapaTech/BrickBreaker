7a3cd68cfabecfe5113c5a1e44a7aebb5bab3584
Jonathan Iha <58711422+JapaTech@users.noreply.github.com>
2024-08-08 14:21:08 -0300

    Brick toma dano
    Adicionado vida ao brick, quando essa vida chega a 0 ele morre

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int health = 1;
+    public static Action OnDeath;
+
+    private void Death()
     {
-        
+        OnDeath?.Invoke();
+        gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TakeDamage()
     {
-        
+        health--;
+
+        if(health <= 0)
+        {
+            health = 0;
+            Death();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            TakeDamage();
+        }
     }
 }
