36967a6bdcd03b3bb8e65f94755054b3ce5db52c
Jonathan Iha <58711422+JapaTech@users.noreply.github.com>
2024-08-09 23:44:29 -0300

    Update UIManager.cs

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,9 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    
+    [SerializeField] private Image lives;
+    [SerializeField] private TMP_Text score;
+
+    public void UpdateHealth(int current, int max)
+    {
+        lives.rectTransform.sizeDelta = new Vector2(lives.rectTransform.sizeDelta.x * current / max, lives.rectTransform.sizeDelta.y);
+    }
+
+    public void UpdateScore(int value)
+    {
+        score.text = "Pontos: " + value.ToString();
+    }
 }
